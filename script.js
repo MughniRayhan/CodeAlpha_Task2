b16a985914ec8dc2e593c50a6e814e32e9475880
MughniRayhan <tishtisha13@gmail.com>
2024-06-22 23:27:26 +0600

    remove  values

--- script.js
+++ script.js
@@ -17,13 +17,26 @@ const equal = () =>{
     const result=String(eval(inputDisplay.value));
     inputDisplay.value=result;
 }
+//remove values
+const acButton = () =>{
+    inputDisplay.value="";
+}
 
+//delete values
+const delButton = () =>{
+    string=string.substring(0,string.length-1);
+     inputDisplay.value=string;
+     console.log(inputDisplay.value)
+     console.log(string)
+ 
+ }
+ 
  //selectButton
  calBtn.forEach((button)=>{
      button.addEventListener("click" , calculate);
    
  })
  eql.addEventListener("click" ,  equal);
- 
-
+ AC.addEventListener("click" ,  acButton);
+ DEL.addEventListener("click" , delButton);
 
